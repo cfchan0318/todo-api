336e29553657bdf05cec0ce6171d5e6bdbe1136a
Chan Chi Fung, Marco <cf.chan0318@gmail.com>
2023-08-26 12:40:32 +0800

    basic crud

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
-const {getTodos, getTodoById} = require('./controllers/todoController')
+const {getTodos, getTodoById, updateTodoById, createTodo} = require('./controllers/todoController')
 
 app.use(express.json())
+
+
+app.post('/todos', async(req, res) => {
+    const todo = req.body;
+    await createTodo(todo);
+    res.status(200).json({"msg":"todo has been created"})
+});
 
 app.get('/todos', async(req, res) => {
     const todos = await getTodos();
@@ -14,12 +21,13 @@ app.get('/todos', async(req, res) => {
 
 app.get('/todos/:todoId', async (req, res) => {
     const todos = await getTodoById(req.params.todoId);
-    res.status(200).json(todos)
+    res.status(200).json(todos[0])
 });
 
 app.put('/todos/:todoId', async (req, res) => {
-    const todo = req.body.todo;
-    
+    const todo = req.body;
+    await updateTodoById(todo);
+    res.status(200).json({"msg":"todo has been updated"})
 });
 
 const port = process.env.port || 3000;
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -21,26 +21,56 @@ const getTodoById = async (toddoId) => {
     }
 }
 
-const updateTodoById = async (todo)=>{
+const createTodo = async (todo)=>{
     const pool = await sql.connect(dbconfig);
-    const query = `UPDATE todo_app.dbo.todo
-                SET title= @title, 
-                description=@desc, 
-                iscomplete=@iscomplete, 
-                WHERE id=@id;
-                `
+    const query = `INSERT INTO todo_app.dbo.todo
+                    (title, description, iscomplete)
+                    VALUES(@title, @description, @iscomplete);
+                    `
     const ps = new sql.PreparedStatement(pool);
-    ps.input('title', sql.VarChar());
-    ps.input('desc', sql.VarChar());
+    ps.input('title', sql.VarChar(4000));
+    ps.input('description', sql.VarChar(4000));
     ps.input('iscomplete', sql.Bit);
     ps.input('id', sql.Int);
 
     await ps.prepare(query);
     await ps.execute({
-
+        'title': todo.title,
+        'description': todo.description,
+        'iscomplete': todo.iscomplete,
+        'id': todo.id
     });
     await ps.unprepare();
 
+    return;
+
 }
 
-module.exports = {getTodos,getTodoById}
+const updateTodoById = async (todo)=>{
+    const pool = await sql.connect(dbconfig);
+    const query = `UPDATE todo_app.dbo.todo
+                SET title= @title, 
+                description=@description, 
+                iscomplete=@iscomplete
+                WHERE id=@id;
+                `
+    const ps = new sql.PreparedStatement(pool);
+    ps.input('title', sql.VarChar(4000));
+    ps.input('description', sql.VarChar(4000));
+    ps.input('iscomplete', sql.Bit);
+    ps.input('id', sql.Int);
+
+    await ps.prepare(query);
+    await ps.execute({
+        'title': todo.title,
+        'description': todo.description,
+        'iscomplete': todo.iscomplete,
+        'id': todo.id
+    });
+    await ps.unprepare();
+
+    return;
+
+}
+
+module.exports = {createTodo,getTodos,getTodoById,updateTodoById}
